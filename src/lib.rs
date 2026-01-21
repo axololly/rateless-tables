978a6b15973ab6de39e3f10e0ed4145463932a8a
axololly <jamesgriggs362@gmail.com>
2026-01-21 02:01:36 +0000

    Deleted bytes.rs and updated version number Turns out RIBLT doesn't work on variable-sized datatypes, so there's no point in having AsBytes<T> and AsVariableBytes<T> in the codebase.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-mod bytes;
 mod decoder;
 mod encoder;
 mod impls;
@@ -8,7 +7,6 @@ mod symbol;
 #[cfg(test)]
 mod tests;
 
-pub use bytes::{AsBytes, AsVariableBytes, Bytes};
 pub use decoder::Decoder;
 pub use encoder::Encoder;
-pub use symbol::{Symbol, CodedSymbol};
+pub use symbol::{CodedSymbol, Symbol};
